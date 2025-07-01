e517a9bd44cf8b52027b0665aa7560efd7f9ec1a
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-01 14:15:15 +0300

    настроила админку

---
+++ webapp/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib import admin
+from webapp.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    fields = ['title', 'description']
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'title', 'price', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['created_at', 'category']
+    search_fields = ['title']
+    fields = ['title', 'price', 'description', 'category', 'created_at', 'updated_at', 'image']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Product, ProductAdmin)
