39a278c3d6ea0d522a7ab4fa050d3f03ae7a6844
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-08 01:31:06 +0300

    добавила для products поле rest, накатила миграцию

--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,4 @@
-
-
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Category(models.Model):
@@ -19,7 +18,12 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     category = models.ForeignKey('webapp.Category', on_delete=models.RESTRICT, verbose_name='Категория', related_name='products')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    #quantity = models.IntegerField(verbose_name='Количество', min_value=0)
+    rest = models.IntegerField(
+        verbose_name='Количество',
+        validators=[MinValueValidator(0)],
+        null=True,
+        blank=True
+    )
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
     image = models.URLField(max_length=300, verbose_name='Картинка')
 
